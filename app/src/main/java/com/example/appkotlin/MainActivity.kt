b8dd2f1973a4daa18041e98b502467ae407b24fe
Luís Henrique <78195873+LuisHenriqueSantos@users.noreply.github.com>
2023-12-13 22:25:46 -0300

    Finalizado o App de sorteio
    Criado um script que a cada interação do botão, vai gerar apenas números inteiros.
    Os número será apenas de 0...10

--- app/src/main/java/com/example/appkotlin/MainActivity.kt
+++ app/src/main/java/com/example/appkotlin/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.appkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(11) // numeros de 0 a 10
+        textoResultado.setText("Número gerado $numero")
+    }
 
 }
